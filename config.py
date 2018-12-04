2c59c83bdcd6272427bcd88a56eab270c9d49394
Manfred Karrer <mk@nucleo.io>
2018-12-04 14:49:54 +0100

    Use getpass.getuser() instead of $USER

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
+import getpass
+
+username = getpass.getuser()
+
 # Bisq directory to get the git commit hash
-bisqHome = "/home/$USER/bisq/bisq/desktop/"
+bisqHome = "/home/"+username+"/bisq/bisq/desktop/"
 
 # Data directory where seednode keeps the json dumps
-dataDir = "/home/$USER/.local/share/dao_test/seed_2002/btc_regtest/db/"
+dataDir = "/home/"+username+"/.local/share/dao_test/seed_2002/btc_regtest/db/"
