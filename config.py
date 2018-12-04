e579356edba7f4773b57bfe85b11b83e63ecbd1b
Manfred Karrer <mk@nucleo.io>
2018-12-04 14:44:33 +0100

    Use $USER instead of hardcoded username

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Bisq directory to get the git commit hash
-bisqHome = "/home/sqrrm/bisq/bisq/desktop/"
+bisqHome = "/home/$USER/bisq/bisq/desktop/"
 
 # Data directory where seednode keeps the json dumps
-dataDir = "/home/sqrrm/.local/share/dao_test/seed_2002/btc_regtest/db/"
+dataDir = "/home/$USER/.local/share/dao_test/seed_2002/btc_regtest/db/"
