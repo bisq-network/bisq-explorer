a0cba48a2754a23b4726b6a6f49f97ede7707b6e
Manfred Karrer <mk@nucleo.io>
2018-12-04 14:54:54 +0100

    Remove json dir from path

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 bisqHome = "/home/user/bisq/bisq"
 
 # Data directory where seednode keeps the json dumps
-dataDir = "/home/user/.local/share/seed_BTC_TESTNET_p66zj5dzhccqhes3/btc_testnet/db/json"
+dataDir = "/home/user/.local/share/seed_BTC_TESTNET_p66zj5dzhccqhes3/btc_testnet/db/"
