2af901afcb6040636fbbac86440780bde0137cf5
sqrrm <sqrrm1@gmail.com>
2018-12-05 00:18:19 +0100

    Copy full data dir

--- update_data.sh
+++ update_data.sh
@@ -2,6 +2,7 @@
 
 DATADIR=/home/user/.local/share/seed_BTC_TESTNET_p66zj5dzhccqhes3/btc_testnet
 
-rsync -r $DATADIR/db/ www/data
+rm -rf www/data
+cp -r $DATADIR/db www/data
 rm -rf www/addr
 python bsq_json.py
