8c416721c033c18adb184e3cba2d6783ba0fc67c
sqrrm <sqrrm1@gmail.com>
2018-12-04 23:48:53 +0100

    Fix address update issue

--- update_data.sh
+++ update_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DATADIR=/home/user/.local/share/seed_BTC_TESTNET_p66zj5dzhccqhes3/btc_testnet/db
-
+DATADIR=/home/user/.local/share/seed_BTC_TESTNET_p66zj5dzhccqhes3/btc_testnet
 
 rsync -r $DATADIR/db/ www/data
+rm -rf www/addr
 python bsq_json.py
