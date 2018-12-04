c00ff7ce244a3cbc94db3d7c263f0f5a5f574fd5
Manfred Karrer <mk@nucleo.io>
2018-12-04 14:54:08 +0100

    Set correct paths
    - remove dynamic user property again as path need to be set anyway...

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
-import getpass
-
-username = getpass.getuser()
-
 # Bisq directory to get the git commit hash
-bisqHome = "/home/"+username+"/bisq/bisq/desktop/"
+bisqHome = "/home/user/bisq/bisq"
 
 # Data directory where seednode keeps the json dumps
-dataDir = "/home/"+username+"/.local/share/seed_BTC_TESTNET_p66zj5dzhccqhes3/"
+dataDir = "/home/user/.local/share/seed_BTC_TESTNET_p66zj5dzhccqhes3/btc_testnet/db/json"
