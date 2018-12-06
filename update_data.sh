ba6826d92150d4c54aada1e8c690a23e688f766f
sqrrm <sqrrm1@gmail.com>
2018-12-06 12:54:46 +0100

    Fix update_data.sh

--- update_data.sh
+++ update_data.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-DATADIR=/home/user/.local/share/seedNode/btc_testnet
+DATADIR=/home/user/.local/share/seed_BTC_TESTNET_p66zj5dzhccqhes3/btc_testnet
 
+cd `dirname $0`
 rm -rf www/data
-cp -r $DATADIR/db www/data
 rm -rf www/addr
+cp -r $DATADIR/db www/data
 python bsq_json.py
