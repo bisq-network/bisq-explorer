e1ee781336493f38803fd174d54e55a8352d6e32
wiz <j@wiz.biz>
2020-11-14 14:44:19 +0900

    As part of shutting down half the legacy BSQ explorers, update redirect

--- docs/random-redirect.js
+++ docs/random-redirect.js
@@ -1,10 +1,7 @@
 var explorers = [
     "https://bsq.ninja",
-    "https://bsq.sqrrm.net",
-    "https://bsq.bisq.services",
-    "https://bsq.vante.me",
     "https://bsq.emzy.de",
-    "https://bsq.bisq.cc",
+    "https://bsq.bisq.services"
 ];
 var explorer = explorers[Math.floor(Math.random() * 1000) % explorers.length];
 document.location.href = explorer + document.location.pathname + document.location.search;
